2d3eb02a2c952cef78483eec62941adefd2450e6
Amalaviya3030 <aryanmalaviya708@gmail.com>
2024-03-29 19:10:01 -0400

    Create an focused assignment 8

---
+++ f8.cpp
@@ -0,0 +1,16 @@
+/*
+ * FILE: f8.cpp
+ * PROJECT : focused Assignment 8
+ * PROGRAMMER : Aryankumar Jitendrabhai Malaviya (8963030)
+ * FIRST VERSION : 2024 - 03 - 29
+ * DESCRIPTION : Write a program to give you practice at working with
+ *               C-style strings, files, command-line arguments, and structs.
+ */
+
+#include <stdio.h>
+
+
+void main()
+{
+    
+}
